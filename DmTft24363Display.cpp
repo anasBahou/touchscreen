e82ccda6df20e9ca32382ca9851c13487d2665f2
6TRON Training <6tron-training@catie.fr>
2019-04-27 18:43:22 +0200

    Add DmTft24363Display.cpp

---
+++ DmTft24363Display.cpp
@@ -0,0 +1,30 @@
+/*
+ * DmTft24363Display.cpp
+ *
+ *  Created on: 15 avr. 2019
+ *      Author: sixtron-training
+ */
+
+#include "mbed.h"
+#include "DmTftIli9341.h"
+#include "DmTouch.h"
+#include "Button.h"
+#include "PinNames.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <DmTft24363Display.h>
+
+DmTft24_363_Display::DmTft24_363_Display(DmTftIli9341* display_controler, DmTouch* touch_controler, InterruptIn* interrupt, FlashIAP* flash) {
+	// TODO Auto-generated constructor stub
+
+}
+
+DmTft24_363_Display::~DmTft24_363_Display() {
+	// TODO Auto-generated destructor stub
+}
+
+void DmTft24_363_Display::init() {
+
+}
+
